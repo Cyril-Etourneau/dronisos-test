091b090763ea454fba7217f2ab082e9e91ef0b5d
Cyril Etourneau <c.etourneau@groupeonepoint.com>
2026-02-27 17:01:02 +0100

    feat: App navigation

--- vue-frontend-boilerplate/src/App.vue
+++ vue-frontend-boilerplate/src/App.vue
@@ -1,8 +1,21 @@
 <template>
     <v-app>
-        <v-app-bar app color="amber darken-1" dark />
+        <v-app-bar app color="amber darken-1">
+            <div class="d-flex align-center drone-info" @click="moveToPage('drone-info')">
+                <v-img src="@/assets/logo.png" alt="Logo" contain max-width="32" class="mr-3" />
+                <v-toolbar-title>Dronisos's drone tracking</v-toolbar-title>
+            </div>
+
+            <v-spacer />
+
+            <v-btn icon @click="moveToPage('hello-world')">
+                <v-icon>mdi-information</v-icon>
+            </v-btn>
+        </v-app-bar>
         <v-main>
-            <router-view />
+            <v-container class="px-4 px-sm-6">
+                <router-view />
+            </v-container>
         </v-main>
     </v-app>
 </template>
@@ -11,5 +24,17 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+    protected moveToPage(name: string) {
+        if (this.$route.name !== name) {
+            this.$router.push({ name });
+        }
+    }
+}
 </script>
+
+<style>
+.drone-info {
+    cursor: pointer;
+}
+</style>
