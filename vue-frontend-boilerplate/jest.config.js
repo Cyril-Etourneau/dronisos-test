ac79656e7eba60ec5da69fc4b3baee1e5bd2534a
Cyril Etourneau <c.etourneau@groupeonepoint.com>
2026-02-27 06:32:52 +0100

    chore: Added jest

---
+++ vue-frontend-boilerplate/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts','tsx','js','json','vue'],
+};
